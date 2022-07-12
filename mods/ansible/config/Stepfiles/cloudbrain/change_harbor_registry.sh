b9d311894c0a9b28f718705d33acfbb905571cfe
linwei wu <liwei.wu@uisee.com>
2022-07-12 17:20:44 +0800

    change to configs

--- mods/ansible/config/Stepfiles/cloudbrain/change_harbor_registry.sh
+++ mods/ansible/config/Stepfiles/cloudbrain/change_harbor_registry.sh
@@ -8,6 +8,9 @@ do
     fi
     sed -i "s&jiashan.uisee.com&$2&g" $i
     sed -i "s&UcloudKubernetes&$3&g" $i
+    if [ $i == 'cloud-nome-latest.yaml.gotmpl' ];then
+       sed -i "s&local-Namespace&$4&g"  $i
+    fi
     #sed -i "s&projectName: dongwu&projectName: $4&g" $i
     #jiashan-dongwu namespace
 done
