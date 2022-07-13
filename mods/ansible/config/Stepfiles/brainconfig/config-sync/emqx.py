9b3180e501751a5ff07948a8686183c8bff127b0
linwei wu <liwei.wu@uisee.com>
2022-07-13 13:30:59 +0800

    console-mysql add init

--- mods/ansible/config/Stepfiles/brainconfig/config-sync/emqx.py
+++ mods/ansible/config/Stepfiles/brainconfig/config-sync/emqx.py
@@ -25,18 +25,22 @@ def upload():
             }
     url=f'http://{sys.argv[1]}:32290/api/v4/data/file'
 
-    result=requests.post(url,headers=header,data=json.dumps(cloud_data))
-    service_logger.info(result.json())
+    cloud_result=requests.post(url,headers=header,data=json.dumps(cloud_data))
     url=f'http://{sys.argv[1]}:32291/api/v4/data/file'
-    result=requests.post(url,headers=header,data=json.dumps(emqx_data))
-    service_logger.info(result.json())
-def import_file():
+    emqx_result=requests.post(url,headers=header,data=json.dumps(emqx_data))
+    try:
+       return  cloud_result.json()['data']['node'],emqx_result.json()['data']['node']
+    except Exception as e:
+       service_logger.info('Data was error  please check the script')
+       raise
+def import_file(cloud_result,emqx_result):
     cloud_import_data={
             'filename':'cloud-emqx-jiashan-prod.json',
-
+            'node':cloud_result
             }
     emqx_import_data={
     'filename':'emqx-jiashan-prod.json',
+        'node':emqx_result
 
             }
     url=f'http://{sys.argv[1]}:32290/api/v4/data/import'
